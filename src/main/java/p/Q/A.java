90f08dba220f8d11713159b0607b3ad86099c96d
prasadscom67 <prasadscom67@gmail.com>
2020-05-08 18:04:17 +0530

    first commit message

---
+++ src/main/java/p/Q/A.java
@@ -0,0 +1,16 @@
+package p.Q;
+
+import org.testng.annotations.Test;
+
+
+public class A {
+	
+	
+	
+	@Test
+	public void AA()
+    {
+        System.out.println("AA");
+    }
+
+}
---
+++ src/test/java/p/Q/B.java
@@ -0,0 +1,13 @@
+package p.Q;
+
+import org.testng.annotations.Test;
+
+public class B {
+	
+	@Test
+	public static void BB()
+    {
+        System.out.println( "BB" );
+    }
+
+}
